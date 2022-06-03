5b478ce0043b9c6d9a8e8226afb6936cb8a6f686
PhilipKorans <06koranteng@gmail.com>
2022-06-02 22:07:37 -0700

    C Prog

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,8 +3,8 @@
 * main - prints the size of various types
 * Return: 0 if exited properly, non-zero otherwise
 */
-int main(void)
+int main (void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
